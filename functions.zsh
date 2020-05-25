43c1166e500fac9781d3f2e1fef09a1d545931b6
The NT <nt@eigh.tk>
2020-05-25 09:32:11 +0200

    gsort gpr ghead replaced with sort pr hehead

--- functions.zsh
+++ functions.zsh
@@ -12,7 +12,7 @@ bindkey '^Xs' run-with-sudo
 
 # Top ten memory hogs
 # http://www.commandlinefu.com/commands/view/7139/top-ten-memory-hogs
-memtop() {ps -eorss,args | gsort -nr | gpr -TW$COLUMNS | ghead}
+memtop() {ps -eorss,args | sort -nr | pr -TW$COLUMNS | head}
 zle -N memtop
 
 tmux-hglog() {
